3269764c0aaa552c63dd9c6ffe352e07621f3ee4
OksanaChe <oksanaadams@yandex.ru>
2022-09-29 22:54:12 +0300

    lang fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-const newLang = "ru";
-document.documentElement.lang = newLang;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
