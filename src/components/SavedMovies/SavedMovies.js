aa4d3d2d88ebddaab69b64f1a1043238f79572c2
OksanaChe <oksanaadams@yandex.ru>
2022-10-15 21:02:59 +0300

    fix: delete unused

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -9,35 +9,12 @@ function SavedMovies({isMovies, savedMovies, isLiked, isMovieLiked, handleMovieD
     const [searchResult, setSearchResult] = useState(localStorage.getItem("liked") ? JSON.parse(localStorage.getItem("liked")) : []);
     const [someMoviesFound, setSomeMoviesFound] = useState(undefined);
 
-  //  useEffect(()=> {
-  //      showSearchResult();
-  //  }, []);
-
     const handleSearchButtonClick = (searchRequest, shortie) => {
         const searchResult = savedMovies.filter((item) => item.nameRU.toLowerCase().includes(searchRequest.toLowerCase()));
         shortie ? setSearchResult(searchResult.filter((item) => item.duration <= 40)) : setSearchResult(searchResult);
         (searchResult.length > 0) ? setSomeMoviesFound(true) : setSomeMoviesFound(false);
         localStorage.setItem("mySavedSearch", JSON.stringify(searchRequest));
     }
-    const showSearchResult = () => {
-
-        setSearchResult(savedMovies);
-        (searchResult.length > 0) ? setSomeMoviesFound(true) : setSomeMoviesFound(false);
-
-        if ((localStorage.getItem("mySavedSearch")) && JSON.parse(localStorage.getItem("mySavedSearch"))!== undefined) {
-            if (JSON.parse(localStorage.getItem("savedShortie")) === true) {
-                const searchResult = (JSON.parse(localStorage.getItem("liked"))).filter((item) => item.nameRU.toLowerCase().includes((JSON.parse(localStorage.getItem("mySavedSearch"))).toLowerCase()));
-                setSearchResult(searchResult.filter((item) => item.duration <= 40));
-                (searchResult.length > 0) ? setSomeMoviesFound(true) : setSomeMoviesFound(false);
-            } else if (JSON.parse(localStorage.getItem("savedShortie")) === false) {
-                const searchResult = (JSON.parse(localStorage.getItem("liked"))).filter((item) => item.nameRU.toLowerCase().includes((JSON.parse(localStorage.getItem("mySavedSearch"))).toLowerCase()));
-                setSearchResult(searchResult);
-            }
-        }
-        localStorage.setItem("savedShortie", false);
-        localStorage.setItem("mySavedSearch", JSON.stringify(''));
-
-   }
     return (
         <div className="saved-movies">
             <HeaderMovies/>
